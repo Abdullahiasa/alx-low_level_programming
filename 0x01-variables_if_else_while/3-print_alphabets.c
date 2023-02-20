151c830cbba8aae3c770f462ed6a1d00145d91b4
Abdullahi <abdullahiasa12@gmial.com>
2023-02-20 14:03:38 -0800

    alx c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - prints alphabet in
+ *upper and lower case
+ *Return: 0 is success 1 is error
+ */
+
+int main(void)
+{
+	int alphABET;
+
+	for (alphABET = 'a'; alphABET <= 'z'; alphABET++)
+	       putchar(alphABET);
+
+	for (alphABET = 'A'; alphABET <= 'Z'; alphABET++)
+		putchar(alphABET);
+
+	putchar('\n');
+	return (0);
+		
+}
