0d28168a66b567c7a92bcfc76262e03e9dc2bfdf
Abdullahi <abdullahiasa12@gmial.com>
2023-02-20 13:38:08 -0800

    alx c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - prints a-z when invoked,
+ *a new line is added at the end
+ *Return: 0 is success 1 is error 
+ */
+int main(void)
+{
+	int alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		putchar(alp);
+
+	for (alp = 'A'; alp <= 'Z'; alp++)
+		putchar(alp);
+
+	putchar('\n');
+	return(0);
+}
