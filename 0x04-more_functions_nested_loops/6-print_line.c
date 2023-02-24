06aaa81fbb70627025a8612a5eee6a790cd4b307
Abdullahi <abdullahiasa12@gmial.com>
2023-02-24 00:42:52 -0800

    alx c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,11 +2,14 @@
 
 /**
  *print_line - drwas a straight line in the terminal
+ *@n: is the parameter that tells how many times the line
+ *should be printed
  */
 
 void print_line(int n)
 {
 	int i;
+
 	for (i = 0; i < n; i++)
 	{
 		_putchar('_');
