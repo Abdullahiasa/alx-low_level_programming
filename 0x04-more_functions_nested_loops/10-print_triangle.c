d18dd0a3de51b8c9a2f25babe7609bd11a3a6d84
Abdullahi <abdullahiasa12@gmial.com>
2023-02-24 09:09:36 -0800

    alx c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ * @size: parameter takes size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; i++)
+			{
+				if (j >= (size - (i + 1)))
+				{
+					_putchar('#');
+				}
+				else
+				{
+					_putchar(" ");
+				}
+			}
+		}
+	}
+}
