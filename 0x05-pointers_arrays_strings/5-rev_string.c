9ebfa7c46f0ae69399f41a06fb1de31f5f6f09cf
Abdullahi <abdullahiasa12@gmial.com>
2023-02-28 12:09:02 -0800

    c pointers one

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,17 +5,23 @@
 void rev_string(char *s)
 {
 	int strlen = 0;
-	char newVar;
+	int temp;
 
 	while (s[strlen] != '\0')
 	{
 		strlen++;
 	}
 
+	int a = 0;
+	int b = strlen - 1;
+
 	for (int i = 0; i < strlen; i++)
 	{
-		newVar = s[i];
-		s[i] = s[strlen - i - 1];
-		s[strlen - i - 1] = newVar;
+		temp = s[b];
+		s[b] = s[a];
+		s[a] = temp;
+
+		a++;
+		b++;
 	}
 }
