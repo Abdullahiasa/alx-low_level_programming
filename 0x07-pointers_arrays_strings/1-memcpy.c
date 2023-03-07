31a1d98fbb30383c8acdbf7120c0ebbb9483fded
Abdullahi <abdullahiasa12@gmial.com>
2023-03-07 13:35:11 -0800

    c pointers three

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,7 +5,8 @@
  * @dest: saves the coppied memory area
  * @src: where the memory area is coppied from
  * @n: fills d byte
- * Return: returns pointer dest value*/
+ * Return: returns pointer dest value
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
