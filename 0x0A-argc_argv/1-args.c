a64cd69ee8a9de78a9426ea429e84b9785b89ea0
Abdullahi <abdullahiasa12@gmial.com>
2023-03-10 12:58:27 -0800

    arg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,27 @@
+#include "main.h"
 #include <stdio.h>
+
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: represents number of comman passed.
+ * @argv: represent command line argm passed .
+ * Return: suucess = 0; otherwise its failure.
+ */
 
 int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+	int i;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", i - 1);
+	}
+
 	return (0);
 }
